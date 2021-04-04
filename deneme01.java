049bb4ed71608187dc6e280838c914f445282d33
Tabanca34 <tabancaselcuk85@gmail.com>
2021-04-05 01:48:14 +0300

    S003

---
+++ deneme01.java
@@ -0,0 +1,17 @@
+package mygitproject;
+
+public class deneme01 {
+
+	public static void main(String[] args) {
+		System.out.println("uzak sunucudaki kodum");// Githup .com daki yeni kodum
+		System.out.println("bu satiri ekledim");
+		System.out.println("Ýstanbulda havalar çok soðuk");
+
+		
+		
+		
+		
+		
+	}
+
+}
