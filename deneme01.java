34bb41ed61b652062cbbb06ed5c683b3135a2909
Tabanca34 <81834126+Tabanca34@users.noreply.github.com>
2021-04-08 00:54:05 +0300

    Update deneme01.java

--- deneme01.java
+++ deneme01.java
@@ -5,11 +5,11 @@ public class deneme01 {
 	public static void main(String[] args) {
 		System.out.println("uzak sunucudaki kodum");// Githup .com daki yeni kodum
 		System.out.println("bu satiri ekledim");
-		System.out.println("Ýstanbulda havalar çok soðuk");
+		System.out.println("Ä°stanbulda havalar Ã§ok soÄŸuk");
 
 		
 		
-		
+		System.out.println("Merhaba Ä°stanbul");
 		
 		
 	}
